981c667b6521d00aa15bd4753972cc0139ba185d
carlosgettier <carlosgettier27@gmail.com>
2020-11-20 19:27:06 -0300

    se agrego render a las rutas

--- carpeta del proyecto/app.js
+++ carpeta del proyecto/app.js
@@ -9,18 +9,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname,"./public")))
 app.listen(3000,()=> console.log("servidor corriendo"))
 app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/home.html"))
+    res.render("products/home")
 })
 
 app.get("/detalleDeProducto",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/detalleDeProducto.html"))
+    res.render("products/detalleDeProducto")
 })
 app.get("/carrito",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/carrito.html"))
+    res.render("products/carrito")
 })
 app.get("/register",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/register.html"))
+    res.render("users/register")
 })
 app.get("/login",function(req,res){
-    res.sendFile(path.join(__dirname,"./views/login.html"))
+    res.render("users/login")
 })
