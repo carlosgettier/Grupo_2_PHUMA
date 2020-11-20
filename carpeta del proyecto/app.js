abf6f66e60138c20e8b08164af20446718e5ea21
carlosgettier <carlosgettier27@gmail.com>
2020-11-20 17:12:04 -0300

    se instalo EJS y se cambio las extenciones de las vistas

--- carpeta del proyecto/app.js
+++ carpeta del proyecto/app.js
@@ -1,6 +1,10 @@
 const express= require("express")
 const app = express()
-const path = require("path")
+const path = require("path");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 
 app.use(express.static(path.join(__dirname,"./public")))
 app.listen(3000,()=> console.log("servidor corriendo"))
