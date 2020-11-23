f193bc55c6dc90f5891d48dae7f434738fb58a87
Juan de la Fuente <juandelafuente@hendercross.com>
2020-11-23 18:05:39 -0300

    Se termina de corregir el CSS de todos los productos. Se prueba con muchos

--- carpeta del proyecto/controllers/productosControllers.js
+++ carpeta del proyecto/controllers/productosControllers.js
@@ -2,6 +2,10 @@ let databaseProducts = [
     {"id":"1","nombre":"zapatilla 1", "descripcion":"desc 1","precio":"$10000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla1.jpg"},
     {"id":"2","nombre":"zapatilla 2", "descripcion":"desc 2","precio":"$12000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla2.jpg"},
     {"id":"3","nombre":"zapatilla 3", "descripcion":"desc 3","precio":"$5000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla3.jpg"},
+    {"id":"4","nombre":"zapatilla 4", "descripcion":"desc 4","precio":"$70000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla4.jpg"},
+    {"id":"1","nombre":"zapatilla 1", "descripcion":"desc 1","precio":"$10000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla1.jpg"},
+    {"id":"2","nombre":"zapatilla 2", "descripcion":"desc 2","precio":"$12000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla2.jpg"},
+    {"id":"3","nombre":"zapatilla 3", "descripcion":"desc 3","precio":"$5000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla3.jpg"},
     {"id":"4","nombre":"zapatilla 4", "descripcion":"desc 4","precio":"$70000","rutaALaImagen":"/imagenesProductosDatabase/zapatilla4.jpg"}
 ];
 
