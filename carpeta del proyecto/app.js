f16e202f2c4f160772f91b5863141e395abba15d
carlosgettier <carlosgettier27@gmail.com>
2020-12-09 14:44:39 -0300

    cambio en puerto de app y en script de package.json

--- carpeta del proyecto/app.js
+++ carpeta del proyecto/app.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 
 app.use(express.static(path.join(__dirname,"./public")))
-app.listen(3000,()=> console.log("servidor corriendo"))
+app.listen(process.env.PORT||3000,()=> console.log("servidor corriendo"))
 app.get("/",function(req,res){
     res.render("products/home")
 })
