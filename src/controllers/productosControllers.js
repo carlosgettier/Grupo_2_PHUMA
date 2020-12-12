bcfee157bec67b769d69bd80715547b695d12195
Juan de la Fuente <juandelafuente@hendercross.com>
2020-12-12 12:55:26 -0300

    Se corrigen vistas para que los diferentes componentes renderizados sean dinamicos
    pendiente agregar logica para levantar los datos del json al controlador

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,21 +1,41 @@
-const fs = require ('fs');
-const path = require ('path')
+const fs = require('fs');
+const path = require('path')
 
-let databaseProducts = fs.readFileSync(path.join(__dirname,'..','database','productos.json'));
+let databaseProducts = fs.readFileSync(path.join(__dirname, '..', 'database', 'productos.json'));
 
 databaseProducts = JSON.parse(databaseProducts);
 
+let producto = {
+    "id": "1",
+    "nombre": "zapatilla 1",
+    "descripcion": "desc 1",
+    "categoria": "remeras",
+    "precio": "$10000",
+    "talles": [32, 33, 37],
+    "rutaALaImagen": "zapatilla1.jpg",
+    "tecnologias": "brabsadasd asddjsa pjpajsd adsdadpoasd poajsdpo japo djpoajdpoja spodj apojdspoapodj poajs dpoaj dposj aposjdpo ajsdpo japosdj poajsdpo ajsdpoj aposdj poajsdpo japosdj poasdj poajsdpo japosd jpoasdj poajsdpo "
+};
+
+let arrayTiles = [
+    {"id":"1","nombre":"zapatilla 1", "descripcion":"desc 1","precio":"$10000","rutaALaImagen":"zapatilla1.jpg"},
+    {"id":"2","nombre":"zapatilla 2", "descripcion":"desc 2","precio":"$12000","rutaALaImagen":"zapatilla2.jpg"},
+    {"id":"3","nombre":"zapatilla 3", "descripcion":"desc 3","precio":"$5000","rutaALaImagen":"zapatilla3.jpg"}
+];
+
+
+
+
 module.exports = {
     'all': function (req, res) {
-        res.render('products/productsAll',{ tilesDeProducto: databaseProducts})
+        res.render('products/productsAll', { tilesDeProducto: databaseProducts })
     },
-    "carrito" : function (req, res){
+    "carrito": function (req, res) {
         res.render("products/carrito")
     },
-    "detalle" : function (req,res){
-        res.render("products/detalleDeProducto")
+    "detalle": function (req, res) {
+        res.render("products/detalleDeProducto", { producto: producto, tilesDeProducto: arrayTiles })
     },
-    "add" : function (req,res){
-        res.render("products/addProduct")  
+    "add": function (req, res) {
+        res.render("products/addProduct")
     }
 }
