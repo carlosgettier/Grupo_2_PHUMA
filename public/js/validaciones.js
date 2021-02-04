0d8c02394000d1b7bb8abdea0c60fe8364cbc4e9
carlosgettier <carlosgettier27@gmail.com>
2021-02-04 19:17:50 -0300

    validacion de contraseña

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -27,4 +27,163 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //--------EMPIEZA CARLOS----------
+let password=document.querySelector("#password")
+let eye= document.querySelector(".eyeIcon")
+
+
+eye.addEventListener("click",function(){
+     eye.classList.toggle("hide")
+     eye.classList.toggle("show")
+     
+        if(password.getAttribute("type")== 'password'){
+         password.setAttribute('type','text')
+        }
+        else{
+            password.setAttribute('type','password')
+        }
+ })
+ password.addEventListener()
