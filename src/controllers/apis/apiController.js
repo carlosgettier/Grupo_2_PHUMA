ab76ba8d1680cb02aa56365306bd0d866495fa2f
FacuPagani <facunpagani@hotmail.com>
2021-03-03 17:15:54 -0300

    Se siguen trabajando apis de procutos y usuarios

--- src/controllers/apis/apiController.js
+++ src/controllers/apis/apiController.js
@@ -15,10 +15,71 @@ module.exports = {
                         ]
             }).then(function (usuarios){
                let respuestaFormateada = { datos:usuarios,
-                cuenta:usuarios.length}
+                cantidad:usuarios.length}
 
                 return res.json(respuestaFormateada)
         })
        
-    }
+    },
+    detail: function (req,res){
+        db.user.findByPk(req.params.id).then(function (usuario){
+            res.json({"nombre" : usuario.nombre,
+            "email" : usuario.email,
+            "imagen" : "/api/users/img/" + usuario.id_usuario
+        })
+        }).catch(function (error){
+            res.send("El usuario solicitado no existe")
+        })
+    },
+    imagen: function (req,res){
+        db.user.findByPk(req.params.id).then( function (imagen){
+           
+            res.render('users/img',{imagen: imagen.imagen})
+        })
+
+            
+        
+    },
+    /*=========PRODUCTOS=========*/ 
+    products: function(req, res) {
+       
+       
+        db.product.findAll({
+            attributes:[["id_producto","id"],
+                        ["nombre","nombre"],
+                        ["descripcion","descripcion"],
+                        ["precio","precio"],
+                        [Sequelize.fn('CONCAT',"/api/products/", Sequelize.col('id_producto') ),'detail']
+
+                    ]
+        }).then(function (producto){
+           let respuestaFormateada = { Productos:producto,
+            cantidad:producto.length}
+
+            return res.json(respuestaFormateada)
+    })
+   
+},
+productDetail: function (req,res){
+    db.product.findByPk(req.params.id).then(function (producto){
+        res.json({"nombre" : producto.nombre,
+        "descripcion" : producto.descripcion,
+        "cantidad" : producto.cantidad,
+        "sexo" : producto.id_sexo,
+        "nombre" : producto.nombre,
+        "imagen" : "/api/products/img/" + producto.id_imagen_principal
+    })
+    }).catch(function (error){
+        res.send("El Producto solicitado no existe")
+    })
+},
+productImagen: function (req,res){
+    db.product.findByPk(req.params.id).then( function (imagen){
+       
+        res.render('users/img',{imagen: imagen.id_imagen_principal})
+    })
+
+        
+    
+}
 }
--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -3,7 +3,15 @@ const router = express.Router()
 
 const apiController = require('../controllers/apis/apiController')
 
+/* ============== USERS ================ */ 
 router.get('/users', apiController.users)
+router.get('/users/:id', apiController.detail)
+router.get('/users/img/:id', apiController.imagen)
+
+/* ============== PRODUCTS ================ */ 
+router.get('/products', apiController.products)
+router.get('/products/:id', apiController.productDetail)
+router.get('/products/img/:id', apiController.productImagen)
 
 
 
